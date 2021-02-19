4e30f0f488145fb9eee6b6b3c8628b851a959af8
Michael Yau <yautt@myumanitoba.ca>
2021-02-19 01:58:24 -0600

    updated workout suggestion feature

--- WorkoutSuggest.java
+++ WorkoutSuggest.java
@@ -3,21 +3,50 @@ public class WorkoutSuggest implements Features {
     private int goal; //"0" for tone up, "1" for build muscle and "2" for loose fat
 
     //Constructor
-    public WorkoutSuggest(int gender, int goal) {
-        this.gender = gender;
-        this.goal = goal;
+    public WorkoutSuggest(User user) {
+        gender = user.getGender();
+        goal = -1;
     }
 
     @Override
     public void getInput() {
         // TODO Auto-generated method stub
-
+        
     }
 
     @Override
     public void getOutput() {
-        // TODO Auto-generated method stub
-
+        if(gender == 0) {
+            if(goal == 0) {
+                // Doc1.pdf
+            }
+            else if(goal == 1) {
+                // Doc2.pdf
+            }
+            else if(goal == 2) {
+                // Doc3.pdf
+            }
+            else {
+                // goal input incorrect
+            }
+        }
+        else if(gender == 1) {
+            if(goal == 0) {
+                // Doc4.pdf
+            }
+            else if(goal == 1) {
+                // Doc5.pdf
+            }
+            else if(goal == 2) {
+                // Doc6.pdf
+            }
+            else {
+                // goal input incorrect
+            }
+        }
+        else {
+            // gender input incorrect 
+        }
     }
     
 }
