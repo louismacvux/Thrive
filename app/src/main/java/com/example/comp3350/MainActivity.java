dabba88a13f19d2fea390a5860d7c3d184bb0ada
Michael Yau <yautt@myumanitoba.ca>
2021-02-21 23:03:54 -0600

    spinner updated

--- app/src/main/java/com/example/comp3350/MainActivity.java
+++ app/src/main/java/com/example/comp3350/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
 
         Intent begin = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(begin);
+
+        Spinner workout_spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> workout_adapter = ArrayAdapter.createFromResource(this, R.array.workouts, android.R.layout.simple_spinner_item);
+        workout_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        workout_spinner.setAdapter(workout_adapter);
+        workout_spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
