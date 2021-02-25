684867d5558c597904da915cbe31a7ed0bad6d20
Michael Yau <yautt@myumanitoba.ca>
2021-02-24 23:44:47 -0600

    Spinner partially working

--- app/src/main/java/com/example/comp3350/WorkoutSuggestion.java
+++ app/src/main/java/com/example/comp3350/WorkoutSuggestion.java
@@ -1,5 +1,6 @@
 package com.example.comp3350;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,9 +12,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class WorkoutSuggestion extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button next;
+    int selected_workout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,27 @@ public class WorkoutSuggestion extends AppCompatActivity implements AdapterView.
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.workout_suggestion);
         //adapter.setDropDownViewResource(R.layout.workout_suggestion);
         //workout_spinner.setAdapter(adapter);
-        workout_spinner.setOnItemSelectedListener(this);
+        workout_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        selected_workout = 0;
+                        break;
+                    case 1:
+                        selected_workout = 1;
+                        break;
+                    case 2:
+                        selected_workout = 2;
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         next = (Button)findViewById(R.id.button_next);
     }
@@ -38,5 +62,13 @@ public class WorkoutSuggestion extends AppCompatActivity implements AdapterView.
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void suggestResult (View view) {
+        Intent pdfViewIntent = new Intent(WorkoutSuggestion.this, ViewPDF.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("goal", selected_workout);
+        pdfViewIntent.putExtras(bundle);
+        startActivity(pdfViewIntent);
     }
 }
